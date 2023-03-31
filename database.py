2c2195eb12e07f99f33c44b98803bc6261a2d44f
maya-halevy <maya.halevy2@gmail.com>
2023-03-31 23:43:19 +0300

    updated the database structure to match the new schema

--- database.py
+++ database.py
@@ -13,81 +13,70 @@ cursor = connection.cursor()
 cursor.execute('CREATE DATABASE recipes')
 
 
-# Create the Recipes table
-cursor.execute('''
-    CREATE TABLE Recipes (
-        id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+# Create the recipes table
+cursor.execute("""
+    CREATE TABLE recipes (
+        id INT NOT NULL AUTO_INCREMENT,
         link VARCHAR(255),
         title VARCHAR(255),
         num_reviews INT,
-        rating INT,
+        rating FLOAT,
         date_published DATETIME,
-        description_id INT NOT NULL,
-        FOREIGN KEY(description_id) REFERENCES Descriptions(id)
-    )
-''')
-
-# Create the Ingredients table
-cursor.execute('''
-    CREATE TABLE Ingredients (
-        id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-        recipe_id INT NOT NULL,
-        ingredient VARCHAR(255),
-        FOREIGN KEY(recipe_id) REFERENCES Recipes(id)
-    )
-''')
-
-# Create the Recipe_Details table
-cursor.execute('''
-    CREATE TABLE Recipe_Details (
-        recipe_id INT PRIMARY KEY NOT NULL,
         prep_time_mins INT,
         cook_time_mins INT,
         total_time_mins INT,
         servings INT,
-        FOREIGN KEY(recipe_id) REFERENCES Recipes(id)
-    )
-''')
-
-# Create the Nutrition_Facts table
-cursor.execute('''
-    CREATE TABLE Nutrition_Facts (
-        recipe_id INT PRIMARY KEY NOT NULL,
         calories INT,
         fat_g INT,
         carbs_g INT,
         protein_g INT,
-        FOREIGN KEY(recipe_id) REFERENCES Recipes(id)
+        PRIMARY KEY (id)
     )
-''')
+""")
 
-# Create the Categories table
-cursor.execute('''
-    CREATE TABLE Categories (
-        id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-        category VARCHAR(255)
+# Create the ingredients table
+cursor.execute("""
+    CREATE TABLE ingredients (
+        id INT NOT NULL AUTO_INCREMENT,
+        recipe_id INT,
+        ingredient VARCHAR(255),
+        PRIMARY KEY (id),
+        FOREIGN KEY (recipe_id) REFERENCES recipes(id)
     )
-''')
+""")
 
-# Create the Descriptions table
-cursor.execute('''
-    CREATE TABLE Descriptions (
-        id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-        description VARCHAR(255)
+# Create the categories table
+cursor.execute("""
+    CREATE TABLE categories (
+        id INT NOT NULL AUTO_INCREMENT,
+        category VARCHAR(255),
+        PRIMARY KEY (id)
     )
-''')
+""")
 
-# Create the Relationship table
-cursor.execute('''
-    CREATE TABLE Relationship (
-        category_id INT NOT NULL,
-        recipe_id INT NOT NULL,
-        FOREIGN KEY(category_id) REFERENCES Categories(id),
-        FOREIGN KEY(recipe_id) REFERENCES Recipes(id),
-        PRIMARY KEY(category_id, recipe_id)
+# Create the instructions table
+cursor.execute("""
+    CREATE TABLE instructions (
+        id INT NOT NULL AUTO_INCREMENT,
+        recipe_id INT,
+        step INT,
+        description VARCHAR(255),
+        PRIMARY KEY (id),
+        FOREIGN KEY (recipe_id) REFERENCES recipes(id)
     )
-''')
+""")
 
+# Create the recipe_category table
+cursor.execute("""
+    CREATE TABLE recipe_category (
+        category_id INT,
+        recipe_id INT,
+        FOREIGN KEY (category_id) REFERENCES categories(id),
+        FOREIGN KEY (recipe_id) REFERENCES recipes(id)
+    )
+""")
+
+# commit changes and close the connection
 connection.commit()
 cursor.close()
 connection.close()
