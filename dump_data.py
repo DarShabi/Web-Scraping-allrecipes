612501eb8827b68a039bce7dde7102ef05c33ece
maya-halevy <maya.halevy2@gmail.com>
2023-04-01 11:41:44 +0300

    created insert_recipe_data function

--- dump_data.py
+++ dump_data.py
@@ -1,7 +1,6 @@
 import pymysql
 
 
-# is this supposed to work with argparse? should it be **kwargs?
 def insert_recipe_data(title, ingredients, recipe_details, num_reviews, rating, nutrition_facts, date_published,
                        categories, link, instructions):
     # Connect to MySQL server
@@ -15,64 +14,57 @@ def insert_recipe_data(title, ingredients, recipe_details, num_reviews, rating, 
     # Create cursor object
     cursor = connection.cursor()
 
-    # Check if recipe already exists in the database
-    query = "SELECT id FROM recipes WHERE title=%s"
-    cursor.execute(query, title)
-    result = cursor.fetchone()
+    # Insert into recipes table
+    cursor.execute("""
+    INSERT INTO recipes (link, title, num_reviews, rating, date_published) VALUES (%s, %s, %s, %s, %s)""",
+                   (link, title, num_reviews, rating, date_published))
 
-    # If recipe already exists, close the connection and return
-    if result:
-        print(f"{title} already exists in the database.")
-        cursor.close()
-        connection.close()
-        return
-
-    # Insert recipe data into the recipes table
-    query = "INSERT INTO recipes (title, link, num_reviews, rating, date_published, prep_time_mins, cook_time_mins, total_time_mins, servings, calories, fat_g, carbs_g, protein_g) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    values = (title, link, num_reviews, rating, date_published, recipe_details.get('Prep Time:', None),
-              recipe_details.get('Cook Time:', None), recipe_details.get('Total Time:', None), recipe_details.get('Servings:', None),
-              nutrition_facts.get('Calories', None), nutrition_facts.get('Fat', None), nutrition_facts.get('Carbs', None),
-              nutrition_facts.get('Protein', None))
-    cursor.execute(query, values)
-
-    # Get the recipe ID
+    # Get the recipe_id of the last inserted row
     recipe_id = cursor.lastrowid
 
-    # Insert ingredients into the ingredients table
+    # Insert into ingredients table
     for ingredient in ingredients:
-        query = "INSERT INTO ingredients (recipe_id, ingredient) VALUES (%s, %s)"
-        values = (recipe_id, ingredient)
-        cursor.execute(query, values)
+        cursor.execute("INSERT INTO ingredients (recipe_id, ingredient) VALUES (%s, %s)", (recipe_id, ingredient))
 
-    # Insert categories into the categories table
-    category_ids = []
+    # Insert into recipe_details table
+    cursor.execute("""
+    INSERT INTO recipe_details (recipe_id, prep_time_mins, cook_time_mins, total_time_mins, servings) 
+    VALUES (%s, %s, %s, %s, %s)""",
+                   (recipe_id, recipe_details['Prep Time:'], recipe_details['Cook Time:'],
+                    recipe_details['Total Time:'], recipe_details['Servings:']))
+
+    # Insert into nutrition_facts table
+    cursor.execute("""
+    INSERT INTO nutrition_facts (recipe_id, calories, fat_g, carbs_g, protein_g) VALUES (%s, %s, %s, "
+                   "%s, %s)""", (recipe_id, nutrition_facts['Calories'], nutrition_facts['Fat'],
+                                 nutrition_facts['Carbs'], nutrition_facts['Protein']))
+
+    # Insert categories and their relationships
     for category in categories:
-        query = "SELECT id FROM categories WHERE category=%s"
-        cursor.execute(query, category)
-        result = cursor.fetchone()
+        # Check if category already exists
+        cursor.execute("SELECT id FROM categories WHERE category = %s", (category,))
+        category_id = cursor.fetchone()
 
-        if result:
-            category_ids.append(result[0])
+        if category_id is None:
+            # Insert new category into categories table
+            cursor.execute("INSERT INTO categories (category) VALUES (%s)", (category,))
+            category_id = cursor.lastrowid
         else:
-            query = "INSERT INTO categories (category) VALUES (%s)"
-            cursor.execute(query, category)
-            category_ids.append(cursor.lastrowid)
+            category_id = category_id[0]
 
-    # Insert recipe and category IDs into the recipe_category table
-    for category_id in category_ids:
-        query = "INSERT INTO recipe_category (category_id, recipe_id) VALUES (%s, %s)"
-        values = (category_id, recipe_id)
-        cursor.execute(query, values)
+        # Insert relationship
+        cursor.execute("""
+        INSERT INTO relationship (category_id, recipe_id) VALUES (%s, %s)", (category_id, recipe_id)""")
 
-    # Insert instructions into the instructions table
+    # Insert into instructions table
     for step, description in instructions.items():
-        query = "INSERT INTO instructions (recipe_id, step, description) VALUES (%s, %s, %s)"
-        values = (recipe_id, step, description)
-        cursor.execute(query, values)
+        cursor.execute("""
+        INSERT INTO instructions (recipe_id, step, description) VALUES (%s, %s, %s)"""
+                       , (recipe_id, step, description))
 
-    # Commit changes and close the connection
+    # Commit the transaction
     connection.commit()
+
+    # Close the connection
     cursor.close()
     connection.close()
-
-    print(f"{title} has been added to the database.")
