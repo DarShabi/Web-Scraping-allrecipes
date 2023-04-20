27bf4b97507e791e41c4cda1b366067e9767d641
“Dar <Sdardar502@gmail.com>
2023-04-20 21:24:17 +0300

    split the insert data func to multiplt insert functions.

--- dump_data.py
+++ dump_data.py
@@ -1,41 +1,51 @@
 import sql_connection as sq
 
 
-def write_to_database(scraped_data):
+def insert_recipe_data(cursor, scraped_data):
     """
-    Write recipe data to the database.
-    :param:scraped_data (dict): A dictionary containing information about a recipe.
-    :return: None
+    Insert recipe data into the recipes table.
+    :param cursor: Cursor object used to execute the query.
+    :param scraped_data: A dictionary containing information about a recipe.
     """
-    connection = sq.sql_connector()
-    cursor = connection.cursor()
-
-    # insert recipe data into recipes table
     sql = "INSERT INTO recipes (link, title, num_reviews, rating, date_published) VALUES (%s, %s, %s, %s, %s)"
     values = (scraped_data['link'], scraped_data['title'], scraped_data['reviews'], scraped_data['rating'],
               scraped_data['published'])
     execute_sql(cursor, sql, values)
 
-    # get the recipe ID from the newly inserted row
-    recipe_id = cursor.lastrowid
 
-    # insert recipe details into recipe_details table
-    details_not_checked = scraped_data['details']
-    details = check_if_keys_exist(details_not_checked, ['Prep Time:', 'Cook Time:', 'Total Time:', 'Servings:'])
+def insert_recipe_details(cursor, recipe_id, details):
+    """
+    Insert recipe details into the recipe_details table.
+    :param cursor: Cursor object used to execute the query.
+    :param recipe_id: The ID of the recipe.
+    :param details: A dictionary containing the recipe details.
+    """
     sql = "INSERT INTO recipe_details (recipe_id, prep_time_mins, cook_time_mins, total_time_mins, servings) " \
           "VALUES (%s, %s, %s, %s, %s)"
     values = (recipe_id, details['Prep Time:'], details['Cook Time:'], details['Total Time:'], details['Servings:'])
     execute_sql(cursor, sql, values)
 
-    # insert nutrition facts into nutrition_facts table
-    nutrition_not_checked = scraped_data['nutrition']
-    nutrition = check_if_keys_exist(nutrition_not_checked, ['Calories', 'Fat', 'Carbs', 'Protein'])
-    sql = "INSERT IGNORE INTO nutrition_facts (recipe_id, calories, fat_g, carbs_g, protein_g) VALUES (%s, %s, %s, %s, %s)"
+
+def insert_nutrition_facts(cursor, recipe_id, nutrition):
+    """
+    Insert nutrition facts into the nutrition_facts table.
+    :param cursor: Cursor object used to execute the query.
+    :param recipe_id: The ID of the recipe.
+    :param nutrition: A dictionary containing the nutrition facts.
+    """
+    sql = "INSERT IGNORE INTO nutrition_facts (recipe_id, calories, fat_g, carbs_g, protein_g) " \
+          "VALUES (%s, %s, %s, %s, %s)"
     values = (recipe_id, nutrition['Calories'], nutrition['Fat'], nutrition['Carbs'], nutrition['Protein'])
     execute_sql(cursor, sql, values)
 
-    # insert categories into categories table and relationship into relationship table
-    categories = scraped_data['category']
+
+def insert_categories(cursor, recipe_id, categories):
+    """
+    Insert categories into the categories table and the relationship into the relationship table.
+    :param cursor: Cursor object used to execute the query.
+    :param recipe_id: The ID of the recipe.
+    :param categories: A list of categories.
+    """
     for category in categories:
         # Check if category already exists in the categories table
         sql = "SELECT id FROM categories WHERE category=%s"
@@ -57,21 +67,64 @@ def write_to_database(scraped_data):
         values = (category_id, recipe_id)
         execute_sql(cursor, sql, values)
 
-    # insert ingredients into ingredients table
-    ingredients = scraped_data['ingredients']
+
+def insert_ingredients(cursor, recipe_id, ingredients):
+    """
+    Insert ingredients into the ingredients table.
+    :param cursor: Cursor object used to execute the query.
+    :param recipe_id: The ID of the recipe.
+    :param ingredients: A list of ingredients.
+    """
     for ingredient in ingredients:
         sql = "INSERT INTO ingredients (recipe_id, ingredient) VALUES (%s, %s)"
         values = (recipe_id, ingredient)
         execute_sql(cursor, sql, values)
 
-    # insert instructions into instructions table
-    instructions = scraped_data['instructions']
+
+def insert_instructions(cursor, recipe_id, instructions):
+    """
+    Insert instructions into the instructions table.
+    :param cursor: Cursor object used to execute the query.
+    :param recipe_id: The ID of the recipe.
+    :param instructions: A dictionary containing the instructions.
+    """
     for step, description in instructions.items():
-        sql = "INSERT INTO instructions (recipe_id, description) VALUES (%s, %s)"
-        values = (recipe_id, description)
+        sql = "INSERT INTO instructions (recipe_id, step, description) VALUES (%s, %s, %s)"
+        values = (recipe_id, step, description)
         execute_sql(cursor, sql, values)
 
-    # commit changes to the database
+
+def write_to_database(scraped_data):
+    """
+    Write recipe data to the database.
+    :param scraped_data: A dictionary containing information about a recipe.
+    :return: None
+    """
+    connection = sq.sql_connector()
+    cursor = connection.cursor()
+
+    insert_recipe_data(cursor, scraped_data)
+
+    # get the recipe ID from the newly inserted row
+    recipe_id = cursor.lastrowid
+
+    details_not_checked = scraped_data['details']
+    details = check_if_keys_exist(details_not_checked, ['Prep Time:', 'Cook Time:', 'Total Time:', 'Servings:'])
+    insert_recipe_details(cursor, recipe_id, details)
+
+    nutrition_not_checked = scraped_data['nutrition']
+    nutrition = check_if_keys_exist(nutrition_not_checked, ['Calories', 'Fat', 'Carbs', 'Protein'])
+    insert_nutrition_facts(cursor, recipe_id, nutrition)
+
+    categories = scraped_data['category']
+    insert_categories(cursor, recipe_id, categories)
+
+    ingredients = scraped_data['ingredients']
+    insert_ingredients(cursor, recipe_id, ingredients)
+
+    instructions = scraped_data['instructions']
+    insert_instructions(cursor, recipe_id, instructions)
+
     connection.commit()
     connection.close()
 
